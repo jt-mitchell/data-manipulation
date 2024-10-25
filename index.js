04d831592b1d5bfe31fbb2131d64786fe290460b
Mitchell <jeffrey.mitchell@accenture.com>
2024-10-25 13:22:13 -0400

    update to index.js

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+// const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const check25 = n1 <= 25 ||n1 <= 25 || n1 <= 25 || n1 <= 25;
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4; //Commented out for part 1 exercise
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && check25 && isUnique;
 
 //Part 1
 
@@ -39,8 +39,18 @@ const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
  //Check if the first number is larger than the last. Cache the result in a variable
  const n1GreaterThanN4 = (n1 > n4);
 
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+
+const chain = (n1 - n2) * n3 // 25 is expected
+const total = chain / n4;   // 1.25 is expected
+
+
+
 
 // Finally, log the results.
+
 console.log(isValid);
 //logs the result of isDivBy5 as a Boolean 
 
@@ -49,6 +59,12 @@ console.log(" The numbers are divisible by 5 is " + isDivby5);
 //logs the result of n1GreaterThanN4 as a Boolean
 
 console.log("The first number is larger than the last number is " + n1GreaterThanN4);
+
+//logs the result of 3 equations
+console.log(chain);
+console.log(total);
+
+
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
